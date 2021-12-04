24c93cbf253eeea22459f04713d700fdba73b0ee
mjwash <mjwash@live.unc.edu>
2021-12-04 14:00:37 -0500

    Login Page working

--- register.js
+++ register.js
@@ -1,16 +1,8 @@
 
 window.addEventListener("load", function () {
-  
     function sendData() {
         const sendRequest = new XMLHttpRequest();
         const signupInfo = new URLSearchParams(new FormData(registerForm));
-
-        sendRequest.addEventListener("error", function (event) {
-            alert('Submission unsuccessful! Please try again.');
-        });
-        sendRequest.addEventListener("load", function (event) {
-            alert('Your account was created!');
-        });
 
         sendRequest.open("POST", "http://localhost:5000/app/new/");
         sendRequest.send(signupInfo);
