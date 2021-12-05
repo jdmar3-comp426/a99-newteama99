87a4d6bf025e9c85944ea194c740017eede82d56
mjwash <mjwash@live.unc.edu>
2021-12-04 21:11:06 -0500

    Added comments

--- delete.js
+++ delete.js
@@ -1,9 +1,11 @@
 
 window.addEventListener("load", function () {
   
+    //Searches URL for ID passed from connecting website
     var url = window.location.href.indexOf("?id");
     var userID = window.location.href.substring(url+4);
     
+    //API delete connector
     async function deleteUser() {
        const response = await fetch('http://localhost:5000/app/delete/user/' + userID, {
            method: 'DELETE'
@@ -13,6 +15,7 @@ window.addEventListener("load", function () {
 
     const registerForm = document.getElementById("signup");
 
+    //Will delete based on logged in ID and return to login
     registerForm.addEventListener("click", function(event) {
         event.preventDefault();
         deleteUser();
--- register.js
+++ register.js
@@ -1,5 +1,7 @@
 
 window.addEventListener("load", function () {
+    
+    //XML Request to Pst a new user to database
     function sendData() {
         const sendRequest = new XMLHttpRequest();
         const signupInfo = new URLSearchParams(new FormData(registerForm));
@@ -8,6 +10,7 @@ window.addEventListener("load", function () {
         sendRequest.send(signupInfo);
     };  
 
+    //Gets User, pass, and email into a form object
     const registerForm = document.getElementById("signup");
 
     registerForm.addEventListener("submit", function(event) {
